5b83f11358977ff912814adef3961899a4e860cd
ZoranYankov3 <zoran_yankov@yahoo.com>
2021-08-06 23:50:12 +0300

    use forChild

--- client/src/app/user/user-routing.module.ts
+++ client/src/app/user/user-routing.module.ts
@@ -57,7 +57,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class UserRoutingModule { }

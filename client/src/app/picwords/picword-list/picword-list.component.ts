de31b202535e3020b2d4c832bec223a45f70eada
ZoranYankov3 <zoran_yankov@yahoo.com>
2021-08-08 00:20:59 +0300

    toggle all PWs list view

--- client/src/app/picwords/picword-list/picword-list.component.ts
+++ client/src/app/picwords/picword-list/picword-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 // import { ActivatedRoute } from '@angular/router';
 import { IPW } from 'src/app/shared/interfaces/picword-interface';
+import { IPWRes } from 'src/app/shared/interfaces/picword-res-interface';
 import { HelpService } from 'src/app/shared/services/help.service';
 // import { IPWRes } from 'src/app/shared/interfaces/picword-res-interface';
 import { PicwordsService } from '../picwords.service';
@@ -13,6 +14,7 @@ import { PicwordsService } from '../picwords.service';
 })
 export class PicwordListComponent implements OnInit {
   picWords: IPW[] | undefined;
+  currentWords: IPW[] | undefined;
   toggle: number = 6;
   // pWCount: number = 6;
 
@@ -29,13 +31,14 @@ export class PicwordListComponent implements OnInit {
 
     this._picword.getAll()
       .subscribe((response: any) => {
-        let shuffle = this._helpService.shuffleArray(response);
-        shuffle = shuffle.slice(0, 6);
+        // let shuffle = this._helpService.shuffleArray(response);
+        // shuffle = shuffle.slice(0, 6);
 
         // Convert IPWRes to IPW
-        shuffle = shuffle.map(x => ({ _id: x._id, word: x.word, pictureUrl: x.pictureUrl }));
-        console.log(shuffle);
-        this.picWords = shuffle;
+        response = response.map((x: IPWRes) => ({ _id: x._id, word: x.word, pictureUrl: x.pictureUrl }));
+        console.log(response);
+        this.picWords = response;
+        this.currentWords = response.slice(0, 6);
       },
         err => {
           // this.notificate = { type: 'error', messages: err };
@@ -43,8 +46,12 @@ export class PicwordListComponent implements OnInit {
   }
 
   toggleView(pws: number) {
+    // for(let i = 0; i < pws; i++) {
+    //   this.
+    // }
     console.log(pws);
     this.toggle = pws;
+    this.currentWords = this.picWords?.slice(0, this.toggle);
   }
 
 }

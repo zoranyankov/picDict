99c61d8b384629025d14f4ae1dfa4361e2a9e6d8
ZoranYankov3 <zoran_yankov@yahoo.com>
2021-08-08 16:35:02 +0300

    add pagination to all PWS page

--- client/src/app/picwords/picword-list/picword-list.component.ts
+++ client/src/app/picwords/picword-list/picword-list.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-// import { ActivatedRoute } from '@angular/router';
 import { IPW } from 'src/app/shared/interfaces/picword-interface';
 import { IPWRes } from 'src/app/shared/interfaces/picword-res-interface';
-import { HelpService } from 'src/app/shared/services/help.service';
-// import { IPWRes } from 'src/app/shared/interfaces/picword-res-interface';
 import { PicwordsService } from '../picwords.service';
 
 @Component({
@@ -13,32 +9,24 @@ import { PicwordsService } from '../picwords.service';
   styleUrls: ['./picword-list.component.css']
 })
 export class PicwordListComponent implements OnInit {
-  picWords: IPW[] | undefined;
-  currentWords: IPW[] | undefined;
+  picWords: IPW[] = [];
+  currentPWs: IPW[] = [];
   toggle: number = 6;
-  // pWCount: number = 6;
 
   constructor(
     private _picword: PicwordsService,
-    private _helpService: HelpService,
-    // private _activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    // let userId = this._activatedRoute.snapshot.params.userId;
-    // console.log(userId);
-    this.picWords = undefined;
+    this.picWords = [];
 
     this._picword.getAll()
       .subscribe((response: any) => {
-        // let shuffle = this._helpService.shuffleArray(response);
-        // shuffle = shuffle.slice(0, 6);
-
         // Convert IPWRes to IPW
         response = response.map((x: IPWRes) => ({ _id: x._id, word: x.word, pictureUrl: x.pictureUrl }));
         console.log(response);
         this.picWords = response;
-        this.currentWords = response.slice(0, 6);
+        this.currentPWs = response.slice(0, 6);
       },
         err => {
           // this.notificate = { type: 'error', messages: err };
@@ -46,12 +34,19 @@ export class PicwordListComponent implements OnInit {
   }
 
   toggleView(pws: number) {
-    // for(let i = 0; i < pws; i++) {
-    //   this.
-    // }
-    console.log(pws);
     this.toggle = pws;
-    this.currentWords = this.picWords?.slice(0, this.toggle);
+    this.currentPWs = this.picWords?.slice(0, this.toggle);
+  }
+
+  showPrev(){
+    let length = this.picWords.length;
+    this.currentPWs = this.picWords.splice(length - this.toggle, length);
+    this.picWords = this.currentPWs.concat(this.picWords);
+  }
+  showNext(){
+    let pastPWs = this.picWords.splice(0, this.toggle);
+    this.picWords = this.picWords.concat(pastPWs);
+    this.currentPWs = this.picWords.slice(0,this.toggle);
   }
 
 }

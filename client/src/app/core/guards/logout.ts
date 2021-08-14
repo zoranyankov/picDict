c010e167749e8af29d8aa353991b2bfdcc4aca30
ZoranYankov3 <zoran_yankov@yahoo.com>
2021-08-15 02:22:24 +0300

    display logout message

--- client/src/app/core/guards/logout.ts
+++ client/src/app/core/guards/logout.ts
@@ -3,6 +3,7 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTr
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { logout } from "src/app/+state/authActions";
+import { success } from "src/app/+state/notifyActions";
 import { AuthService } from "src/app/user/auth.service";
 
 @Injectable()
@@ -12,7 +13,8 @@ export class Logout implements CanActivate {
         const {data : {paramsActivateRedirectUrl}} = route;
         localStorage.removeItem('sid');
         this._auth.authenticateUser(null);
-        this._store.dispatch(logout())
+        this._store.dispatch(logout());
+        this._store.dispatch(success({messages:[{message: 'You leave successfully'}]}))
 
         return this.router.parseUrl(paramsActivateRedirectUrl || '/');
         // throw new Error("Method not implemented.");
